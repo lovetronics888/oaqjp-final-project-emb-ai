659a6d14e543f1ffc246a7353cb935a19efc7fd4
lovetronics888 <lovesaumeat@gmail.com>
2026-04-24 20:22:24 -0400

    Fixed the original folder structure.Now my server runs properly

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+import os
+print(f"Current Working Directory: {os.getcwd()}")
+print(f"Template folder exists here: {os.path.exists('templates/index.html')}")
+
+#Flask instance
+app = Flask(__name__, template_folder='templates')
+@app.route("/emotionDetector")
+def emot_detector():
+    """
+    Fetch text from requests,detect emotion and return formatted result
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    if not text_to_analyze:
+        return "Invalid input! Please provide text to analyze."
+    response = emotion_detector(text_to_analyze)
+    formatted_output = (
+    f"For the given statement, the system response is 'anger': {response['anger']}, "
+    f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+    f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+    f"The dominant emotion is {response['dominant_emotion']}."
+)
+
+
+    return formatted_output
+
+@app.route('/')
+def render_index_page():
+    """renders the home page"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
